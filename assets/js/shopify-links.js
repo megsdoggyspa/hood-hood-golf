cf6272d6e7e0ebcc2123413735ddecc65c4fc153
megsdoggyspa <megsdoggyspa@gmail.com>
2026-05-23 22:29:59 -0700

    Add files via upload

---
+++ assets/js/shopify-links.js
@@ -0,0 +1,23 @@
+﻿window.HHG_SHOPIFY_LINKS = {
+  single: "https://www.hoodhoodgolf.com/cart/50188778897601:1?checkout",
+  twosome: "https://www.hoodhoodgolf.com/cart/50188746260673:1?checkout",
+  threesome: "https://www.hoodhoodgolf.com/cart/50188739608769:1?checkout",
+  foursome: "https://www.hoodhoodgolf.com/cart/50141774676161:1?checkout",
+  spectator: "https://www.hoodhoodgolf.com/cart/50190795342017:1?checkout",
+  checkout: "https://www.hoodhoodgolf.com/cart",
+
+  donate: "https://www.hoodhoodgolf.com/cart/50190780530881:1?checkout",
+  sponsor_title: "https://www.hoodhoodgolf.com/cart/50141780541633:1?checkout",
+  sponsor_gold: "https://www.hoodhoodgolf.com/cart/50141780246721:1?checkout",
+  sponsor_silver: "https://www.hoodhoodgolf.com/cart/50141779361985:1?checkout",
+  sponsor_tee_prize: "https://www.hoodhoodgolf.com/cart/50141779001537:1?checkout",
+  sponsor_ball: "https://www.hoodhoodgolf.com/cart/50144887570625:1?checkout",
+  sponsor_drink_cart: "https://www.hoodhoodgolf.com/cart/50141778313409:1?checkout",
+  sponsor_breakfast: "https://www.hoodhoodgolf.com/cart/50141776314561:1?checkout",
+  sponsor_lunch: "https://www.hoodhoodgolf.com/cart/50188729614529:1?checkout",
+  sponsor_water: "https://www.hoodhoodgolf.com/cart/50141777690817:1?checkout",
+  sponsor_longest_drive: "https://www.hoodhoodgolf.com/cart/50141775855809:1?checkout",
+  sponsor_closest_pin: "https://www.hoodhoodgolf.com/cart/50141775462593:1?checkout",
+  sponsor_tee_signage: "https://www.hoodhoodgolf.com/cart/50141775397057:1?checkout",
+  sponsor_hole: "https://www.hoodhoodgolf.com/cart/50141775134913:1?checkout"
+};
